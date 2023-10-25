2119da4ca3e1432484e419498ebe0c1b24d76160
prathibha-yadav <prathibha@LAPTOP-NAAMUCTV>
2023-10-25 17:13:25 +0530

    repo structured

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
+
 
 export default defineConfig({
-  build : {
-    outDir : "dev-dist",
-  },
+  
   plugins: [
     react(),
-    VitePWA({
-      devOptions: {
-        enabled: true 
-      },
-    })
+   
   ],
 })
