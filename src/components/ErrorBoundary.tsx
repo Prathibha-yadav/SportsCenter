d186c450d992d7dae5439555f2d2415b818b6f63
prathibha-yadav <prathibha@LAPTOP-NAAMUCTV>
2023-10-25 17:28:13 +0530

    netlify update

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+  static getDerivedStateFromError(): ErrorBoundaryState {
     return { hasError: true };
   }
 
